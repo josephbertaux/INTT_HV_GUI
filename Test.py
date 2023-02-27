55271c7d750f7c3fbec4b828bfa71cac4f09f736
josephbertaux <jbertau@purdue.edu>
2023-02-27 12:39:58 -0500

    Added more commands to the Wrapper

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ from Wrapper import HV_Wrapper
 
 hv_wrapper = HV_Wrapper()
 
-my_args = {'ip': '1234.5678.9101112', 'channel': '1253215235616', 'f': '100'}
+my_args = {'ip': '1234.5678.9101112', 'channel': '1253215235616', 'value': '100'}
 
 hv_wrapper.TryCommand('set_v', my_args)
 hv_wrapper.TryCommand('get_v', my_args)
--- Wrapper.py
+++ Wrapper.py
@@ -3,16 +3,53 @@
 
 class HV_Wrapper():
     def __init__(self):
-        self.commands = [
-                {'cmd': 'set_v',        'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputVoltage.{channel} F {voltage}'},
-                {'cmd': 'get_v',        'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputVoltage.{channel}'}]
+        self.commands =
+        [
+            # Voltage SNMP commands
+            {'cmd': 'get_v',            'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputMeasurementSenseVoltage.u{channel}'},
+            {'cmd': 'set_v',            'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputVoltage.u{channel} F {value}'},
+            {'cmd': 'get_set_v',        'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputVoltage.u{channel}'},
+
+            {'cmd': 'get_min_v',        'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputSupervisionMinSenseVoltage.u{channel}'},
+            {'cmd': 'set_min_v',        'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputSupervisionMinSenseVoltage.u{channel} F {value}'},
+            {'cmd': 'get_max_v',        'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputSupervisionMaxSenseVoltage.u{channel}'},
+            {'cmd': 'set_max_v',        'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputSupervisionMaxSenseVoltage.u{channel} F {value}'},
+            
+            # Current SNMP commands
+            {'cmd': 'get_i',            'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputMeasurementCurrent.u{channel}'},
+            {'cmd': 'set_i',            'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputCurrent.u{channel} F {value}'},
+            {'cmd': 'get_set_i',        'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputCurrent.u{channel}'},
+
+            {'cmd': 'get_max_i',        'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputSupervisionMaxCurrent.u{channel}'},
+            {'cmd': 'set_max_i',        'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputSupervisionMaxCurrent.u{channel} F {value}'},
+
+            # (Voltage) Rise/Fall Rate SNMP commands
+            ### Not sure how useful these will be
+            {'cmd': 'set_v_rise',       'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputVoltageRiseRate.u{channel} F {value}'},
+            {'cmd': 'get_set_v_rise',   'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputVoltageRiseRate.u{channel}'},
+            
+            {'cmd': 'set_v_fall',       'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputVoltageFallRate.u{channel} F {value}'},
+            {'cmd': 'get_set_v_fall',   'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputVoltageFallRate.u{channel}'},
+
+            # (Current) Rise/Fall Rate SNMP commands
+            ### Not sure how useful these will be
+            ### Fuck it, I'm thorough on principle
+            {'cmd': 'set_i_rise',       'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputCurrentRiseRate.u{channel} F {value}'},
+            {'cmd': 'get_set_i_rise',   'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputCurrentRiseRate.u{channel}'},
+            
+            {'cmd': 'set_i_fall',       'args':'snmpset -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputCurrentFallRate.u{channel} F {value}'},
+            {'cmd': 'get_set_i_fall',   'args':'snmpget -OqvU -v 2c -m +WIENER-CRATE-MIB -c guru {ip} outputCurrentFallRate.u{channel}'}
+        ]
+
 
     def TryCommand(self, cmd, arg_map):
-        # cpp has a library standard std::map which can lookup an item in log time and return an iterator to it
-        # linear time goes brrrrr
-        # fucking normoid language for baby programmers
+        # Could be log * log but I'm not making a dictionary of dictionaries
+        # Enjoy your for loop, not that many elements in the first place
+        # Also python isn't worth optimizing lmao
         for i in self.commands:
             if i['cmd'] == cmd:
+                # reference for how I'm using the format command:
+                # https://docs.python.org/3/library/string.html#format-examples
                 print(i['args'].format(**arg_map))
                 return
         print('cmd "' + cmd + '" not found in dictionary')
